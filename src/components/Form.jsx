4525635998b51ef3dc235d439ed438105e015ace
Paul De Ulloa <pauld143@gmail.com>
2023-09-04 15:46:36 -0700

    edits made to return for Form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -15,6 +15,21 @@ const TodoForm = (props) => {
 
 
     return(
+        <>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Task: </label>
+                    <input class="form-control" name='task' type='text' value={task} onChange={(e) => setTask(e.target.value)} />
+                </div>
+                <div>
+                    <label>Task Completed: </label>
+                    <input class="form-control" name='completed' type='checkbox' onChange={(e) => setCompleted(e.target.checked)} />
+                </div>
+                <div>
+                    <button type='submit' className='btn btn-primary'>Add</button>
+                </div>
+            </form>
+        </>
 
     )
 
