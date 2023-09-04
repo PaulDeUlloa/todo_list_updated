6d4ef2057d23f4d0841304a1034c1fba41a891e5
Paul De Ulloa <pauld143@gmail.com>
2023-09-04 16:20:01 -0700

    List edits made

---
+++ src/components/List.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const List = (props) => {
+
+    const handleDelete = (targetIdx) => {
+        props.deleteTask(targetIdx);
+    }
+
+    const handleUpdate = (targetIdx, targetBool) => {
+        props.updateTask(targetIdx, targetBool)
+    }
+
+    return (
+        <div>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Task</th>
+                        <th>Test</th>
+                        <th>Task Completed</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        props.list.map((eachTodo, Idx) => {
+                                return (
+                                    <tr key={Idx} style={eachTodo.completed ? { backgroundColor: "#08e600" } : { backgroundColor: "#ffffff;" }}>
+                                        <td>{eachTodo.task}</td>
+                                        <td>
+                                            <button class="btn btn-danger" onClick={() => handleDelete(Idx)}>Delete</button>
+                                        </td>
+                                        <td>
+                                            <input type="checkbox" onChange={(e) => handleUpdate(Idx, e.target.checked)} />
+                                        </td>
+                                    </tr>
+                                )
+                            }
+                        )
+                    }
+                </tbody>
+            </table >
+        </div>
+    )
+}
+
+export default List;
