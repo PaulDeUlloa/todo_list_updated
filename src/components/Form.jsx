03fcd61002089edbbd6784a3ac8dda77e8ccf3b3
Paul De Ulloa <pauld143@gmail.com>
2023-09-04 15:26:15 -0700

    initial commit for form.jsx

---
+++ src/components/Form.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useState } from 'react'
+
+const TodoForm = (props) => {
+
+    const [task, setTask] = useState("");
+
+    const [completed, setCompleted] = useState(false);
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+    }
+
+
+    return(
+
+    )
+
+}
+export default Form;
--- src/views/Container.jsx
+++ src/views/Container.jsx
@@ -1,4 +1,25 @@
 import React, { useState } from "react";
-import Form from "./components/Form";
-import List from "./components/List";
+import Form from "../components/Form";
+import List from "../components/List";
 
+const Container = () =>{
+
+    const [list, setList] = useState([]);
+
+    const addToDo = (newToDo) =>{
+        setList([...list, newToDo])
+        console.log (list);
+    }
+
+    
+    return (
+        <div>
+            <h1>User Todo List</h1>
+            <Form addToDo={addToDo} />
+            <list list={list} />
+        </div>
+    )
+
+
+}
+export default Container;
